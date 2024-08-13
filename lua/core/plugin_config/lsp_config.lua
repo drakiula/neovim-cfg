ac8e26bab8747dff463c74e8df049260994eeea5
Dragos - Ioan Haiduc <dragos.haiduc@esolutions.ro>
2024-08-13 14:00:01 +0300

    mason lsp config lua_lsp

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -12,6 +12,25 @@ local on_attach = function(_, _)
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local mason_lspconfig = require("mason-lspconfig")
+local servers = {
+  ruby_lsp = {}
+}
+
+mason_lspconfig.setup {
+  ensure_installed = vim.tbl_keys(servers),
+}
+
+mason_lspconfig.setup_handlers {
+  function (server_name)
+    require("lspconfig")[server_name].setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = servers[server_name],
+      filetypes = (servers[server_name] or {}).filetypes,
+    }
+  end
+}
 
 require("lspconfig").lua_ls.setup {
   on_attach = on_attach,
@@ -26,11 +45,6 @@ require("lspconfig").lua_ls.setup {
 }
 
 require("lspconfig").tsserver.setup {
-  on_attach = on_attach,
-  capabilities = capabilities
-}
-
-require("lspconfig").ruby_lsp.setup {
   on_attach = on_attach,
   capabilities = capabilities
 }
